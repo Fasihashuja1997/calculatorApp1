f6363848aed2bdf0b4818d0c99819197f58fecbf
fasiha <fasiha2555@gmail.com>
2021-07-08 23:29:35 +0500

    work completed

---
+++ app.js
@@ -0,0 +1,13 @@
+function getNum(num) {
+    var answer=document.getElementById("answer");
+    answer.value += num;
+}
+
+function clrAnswer(){
+  var answer=document.getElementById("answer");
+    answer.value = ""
+}
+function getAnswer(){
+  var answer=document.getElementById("answer");
+  answer.value = eval(answer.value)
+}
